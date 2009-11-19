fde9d1991aec530c05f0c6b9fdd0d27d5dd0b291
George Ornbo <george@lewis.local>
2009-11-19 07:32:51 +0000

    initial commit for EE2.0 version

--- pi.friendly_404.php
+++ pi.friendly_404.php
@@ -11,7 +11,7 @@
  * 
  * @category   Plugins
  * @package    friendly_404
- * @version    1.0.0
+ * @version    2.0.0
  * @since      1.0.0
  * @author     George Ornbo <george@shapeshed.com>
  * @see        {@link http://github.com/shapeshed/friendly_404.ee_addon/} 
@@ -19,13 +19,13 @@
  */
 
 $plugin_info = array(
-						'pi_name'			=> 'Friendly 404',
-						'pi_version'		=> '1.1.0',
-						'pi_author'			=> 'George Ornbo',
-						'pi_author_url'		=> 'http://shapeshed.com/',
-						'pi_description'	=> 'Returns suggestions for 404 page based on the final segment of the 404 URL',
-						'pi_usage'			=> Friendly_404::usage()
-					);
+  'pi_name'			=> 'Friendly 404',
+  'pi_version'		=> '2.0.0',
+  'pi_author'			=> 'George Ornbo',
+  'pi_author_url'		=> 'http://shapeshed.com/',
+  'pi_description'	=> 'Returns suggestions for 404 page based on the final segment of the 404 URL',
+  'pi_usage'			=> Friendly_404::usage()
+);
 
 /**
  * Friendly 404 Plugin
@@ -34,123 +34,119 @@ $plugin_info = array(
  * @package    friendly_404
  */
 class Friendly_404{
-	
-	/**
-	* Return data
-	* @var string
-	*/
-	var $return_data;
-	
-	/** 
-	* Returns suggestions for 404 page based on the final segment of the 404 URL
-	* 
-	* @access public 
-	* @return string 
-	*/
-	function Friendly_404() 
+
+  /**
+  * Return data
+  * @var string
+  */
+  var $return_data;
+
+  /** 
+  * Returns suggestions for 404 page based on the final segment of the 404 URL
+  * 
+  * @access public 
+  * @return string 
+  */
+  function Friendly_404() 
     {
 
-		global $TMPL, $DB, $IN, $FNS, $REGX;	
+    $this->EE =& get_instance();	
 
-		/*---------------------------------------
-		Get variables
-		----------------------------------------*/
+    /*---------------------------------------
+    Get variables
+    ----------------------------------------*/
 
-		$search_segment = end($IN->SEGS);
-		$limit = ( ! $TMPL->fetch_param('limit')) ? '5' : $TMPL->fetch_param('limit');
-		$weblog = ( ! $TMPL->fetch_param('weblog')) ? '' : $TMPL->fetch_param('weblog');
+    $search_segment = end($this->EE->uri->segment_array());
 
-		/*---------------------------------------
-		Build weblog query
-		----------------------------------------*/						
+    $this->limit = ( ! $this->EE->TMPL->fetch_param('limit')) ? 5 :  $this->EE->TMPL->fetch_param('limit');
+    $this->channel = ( ! $this->EE->TMPL->fetch_param('channel')) ? '' :  $this->EE->TMPL->fetch_param('channel');
+    $this->tagdata =  $this->EE->TMPL->tagdata;
 
-		$weblog_str = "";
-		if ($weblog != "") 
-		{
-			$count = 1; 
-			$weblogs = explode("|", $weblog);
-				foreach ($weblogs as $weblog_name) 
-				{
-					if ($count == 1) 
-					{
-						$weblog_str .= " AND ( w.blog_name = '".$DB->escape_str($weblog_name)."'";
-						$count++;
-					}
-					else 
-					{
-						$weblog_str .= " OR w.blog_name = '".$DB->escape_str($weblog_name)."'";
-					}
-				} 
-			$weblog_str .= " )";
-		} 	
-			
-		/*---------------------------------------
-		Query the DB
-		----------------------------------------*/			
+    /*---------------------------------------
+    Build channel query
+    ----------------------------------------*/
 
-	    $query = $DB->query("SELECT t.entry_id, t.title, t.url_title, t.weblog_id, w.search_results_url, w.blog_url FROM exp_weblog_titles AS t
-					LEFT JOIN exp_weblogs AS w ON t.weblog_id = w.weblog_id 
-					WHERE t.entry_date < UNIX_TIMESTAMP()
-					$weblog_str
-					AND (t.expiration_date = 0 || t.expiration_date > UNIX_TIMESTAMP())
-					AND (t.title LIKE '%".$DB->escape_str($search_segment)."%')
-					AND t.status = 'Open' AND t.status != 'closed'
-					ORDER BY t.sticky desc, t.entry_date desc, t.entry_id desc
-					LIMIT 0, ".$DB->escape_str($limit)."");
-			
-		$total_results = sizeof($query->result);
-	
-        foreach ($query->result as $count => $row)
+    $channel_str = "";
+    if ($this->channel != "") 
+    {
+      $count = 1; 
+      $channels = explode("|", $channels);
+      foreach ($channel as $channel_name) 
+      {
+        if ($count == 1) 
         {
-			$tagdata = $TMPL->tagdata;
+          $channel_str .= " AND ( w.channel_name = '".$DB->escape_str($channel_name)."'";
+          $count++;
+        }
+        else 
+        {
+          $channel_str .= " OR w.channel_name = '".$DB->escape_str($channel_name)."'";
+        }
+      } 
+      $channel_str .= " )";
+    }
+    
+    /*---------------------------------------
+    Query the DB
+    ----------------------------------------*/
 
-			$row['count']			= $count+1;
-			$row['total_results']	= $total_results;
+    $query = $this->EE->db->query("SELECT t.entry_id, t.title, t.url_title, t.channel_id, c.search_results_url, c.channel_url FROM exp_channel_titles AS t
+    LEFT JOIN exp_channels AS c ON t.channel_id = c.channel_id 
+    WHERE t.entry_date < UNIX_TIMESTAMP()
+    $channel_str
+    AND (t.expiration_date = 0 || t.expiration_date > UNIX_TIMESTAMP())
+    AND (t.title LIKE '%".$this->EE->db->escape_str($search_segment)."%')
+    AND t.status = 'Open' AND t.status != 'closed'
+    ORDER BY t.sticky desc, t.entry_date desc, t.entry_id desc
+    LIMIT 0, ".$this->EE->db->escape_str($this->limit)."");
 
-			/*---------------------------------------
-			Format the link and add to $row[] array as auto_path
-			----------------------------------------*/				
-	
-			$url = ($row['search_results_url'] != '') ? $row['search_results_url'] : $row['blog_url'];							
-			$row['auto_path'] = $FNS->remove_double_slashes($REGX->prep_query_string($url).'/'.$row['url_title'].'/');
 
-			/*---------------------------------------
-			Conditionals
-			----------------------------------------*/
+    if ($query->num_rows() > 0)
+    {
+      $total_results = $query->num_rows();
+      foreach ($query->result_array() as $count => $row)
+      {
+        $row['count']			= $count+1;
+        $row['total_results']	= $total_results;
+        
+        /*---------------------------------------
+        Format the link and add to $row[] array as auto_path
+        ----------------------------------------*/
+        
+        $url = ($row['search_results_url'] != '') ? $row['search_results_url'] : $row['channel_url'];
+        $row['auto_path'] = $this->EE->functions->remove_double_slashes($this->EE->functions->prep_query_string($url).'/'.$row['url_title'].'/');
 
-			$tagdata = $FNS->prep_conditionals($tagdata, $row);
-	
-			/*---------------------------------------
-			Single variables
-			----------------------------------------*/
-	
-		    foreach ($TMPL->var_single as $key => $val)
-		    {
-		        if (isset($row[$val]))
-		        {
-		            $tagdata = $TMPL->swap_var_single($val, $row[$val], $tagdata);
-		        }
-		    }
+        /*---------------------------------------
+        Conditionals
+        ----------------------------------------*/
+        
+        $tagdata =  $this->EE->functions->prep_conditionals($this->tagdata, $row);    
 
-		    $this->return_data .= $tagdata; 
-		}	
-					
-	}
+        /*---------------------------------------
+        Single variables
+        ----------------------------------------*/
+        
+        foreach ($this->EE->TMPL->var_single as $key => $val)
+        {
+          if (isset($row->$val))
+          {
+              $tagdata = $this->EE->TMPL->swap_var_single($val, $row[$val], $tagdata);
+          }
+        } 
+      $this->return_data .= $tagdata;      
+      } 
+    }	
+  }
 
-function usage()
-{
-ob_start(); 
-?>
-See http://github.com/shapeshed/friendly_404.ee_addon/
-
-<?php
-$buffer = ob_get_contents();
-
-ob_end_clean(); 
-
-return $buffer;
-}
-/* END */
+  /**
+  * Plugin usage documentation
+  *
+  * @return	string Plugin usage instructions
+  */
+  public function usage()
+  {
+    return "Documentation is available here http://shapeshed.github.com/expressionengine/plugins/friendly_404/";
+  }
 	
 }
 
